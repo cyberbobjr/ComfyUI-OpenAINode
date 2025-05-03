560d60fa329b04b93487b0ef9f659c6760052b5e
cyberbobjr <cyberbobjr@yahoo.com>
2025-05-03 18:51:34 +0200

    fix

--- openai_node.py
+++ openai_node.py
@@ -12,7 +12,7 @@ class OpenAINode:
                 }),
                 "api_url": ("STRING", {
                     "multiline": False,
-                    "default": "http://127.0.0.1:5000/v1"
+                    "default": "https://api.openai.com/v1/models"
                 }),
                 "api_key": ("STRING", {
                     "multiline": False,
@@ -28,11 +28,23 @@ class OpenAINode:
                 }),
                 "sys_prefix": ("STRING", {
                     "multiline": True,
-                    "default": "You are a prompt generation AI. your task is to take a user input for a stable difusion prompt and output and expand the supplied prompt in a stable difusion format to provide better output. Do not deviate from the format. Do not output anything other than a stable diffusion prompt."
+                    "default": """
+You are an AI assistant specialized in generating high-quality, creative, and detailed prompts for 1D Flow in ComfyUI. Your goal is to create vivid and immersive descriptions of scenes. Each prompt should be a fluid and natural textual description without any section titles, formatting elements, or mentions of ComfyUI or 1D Flow.
+Ensure that each prompt:
+Clearly establishes the setting, atmosphere, and mood.
+Provides rich and evocative details about the visual composition, lighting, textures, and colors.
+Describes characters, their expressions, posture, clothing, and interactions naturally within the scene.
+Reads as a seamless and engaging narrative rather than a structured list.
+Be precise, imaginative, and avoid ambiguity. Generate a single, cohesive scene per request.
+                    """
                 }),
                 "stop_token": ("STRING", {
                     "multiline": False,
                     "default": "<|im_end|>"
+                }),
+                "model": ("STRING", {
+                    "multiline": False,
+                    "default": "gpt-4o-mini"
                 }),
                 "max_tokens": ("INT", {
                         "default": 250,
@@ -54,23 +66,23 @@ class OpenAINode:
 
     CATEGORY = "OpenAIapi"
 
-    def get_completion(self, prompt, api_url, api_key, temperature, sys_prefix, stop_token, max_tokens, seed, model="local model"):
+    def get_completion(self, prompt, api_url, api_key, temperature, sys_prefix, stop_token, max_tokens, seed, model="gpt-4o-mini"):
         try:
-            openai.api_base = api_url
+            openai.base_url = api_url
             openai.api_key = api_key
 
-            
+            client = openai.OpenAI(api_key=api_key, base_url=api_url)
+            print(f"base_url: {api_url}")
             messages = [{"role": "system", "content": sys_prefix},{"role": "user", "content": prompt}]
             
-            response = openai.ChatCompletion.create(
+            response = client.chat.completions.create(
                 model=model,
                 messages=messages,
                 temperature=temperature,
                 max_tokens=max_tokens,
                 stop=stop_token,
             )
-
-            return (response.choices[0].message["content"],)
+            return (response.choices[0].message.content,)
 
         except Exception as e:
             error_message = f"Error: {str(e)}"
